d9ebc760ed5c09024d3f4074adcb19731430dda6
Rafael Gianini Sardinha <scan.sardinha@voeazul.com.br>
2023-09-16 17:13:18 -0300

    Criado Todo em Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public mode: string = 'list'
+  public todos: Todo[] = [];
+  public titulo: String = 'Minhas Tarefas';
+  public form: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      descricao: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+   
+    this.load();
+   
+  }
+  //  alterarTexto () {
+  //   this.titulo = "Teste";
+  // }
+  clear(){
+    this.form.reset();
+  }
+
+  add(){
+    const descricao = this.form.controls['descricao'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, descricao, false));
+    this.save();
+    this.clear();
+  }
+
+  remove (todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  markAsDone (todo: Todo){
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone (todo: Todo){
+    todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  }
+
+  load(){
+    const data = localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data);
+    }else {
+      this.todos = [];
+    }
+  }
+
+  changeMode(mode: string){
+    this.mode = mode;
+  }
+
+  
+    
+
 }
+
---
+++ src/models/todo.model.ts
@@ -0,0 +1,15 @@
+export class Todo {
+  /**
+   *
+   */
+  constructor(
+      public id: Number,
+      public title: String,
+      public done: Boolean,
+  ) {
+      
+      
+  }
+
+
+}
